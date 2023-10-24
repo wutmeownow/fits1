b86ea45f2a455760e16bb4c94b3c00c9488adfcf
Bob Hirosky <hirosky@virginia.edu>
2023-10-23 22:00:43 -0400

    WIP

--- fit1.C
+++ fit1.C
@@ -7,6 +7,7 @@
 
 
 // fit1.C
+// entries is the number of random samples filled into the histogram
 void fit1(int entries=1000, bool save=false) {
    //Simple histogram fitting examples
   gROOT->Reset();  // useful to reset ROOT to a cleaner state
@@ -14,7 +15,7 @@ void fit1(int entries=1000, bool save=false) {
   TFile *tf=0;
   if (save) tf=new TFile("histo.root","recreate");
 
-  TH1F *randomHist1 = new TH1F("randomHist1", "Random Histogram", 100, 0, 100);
+  TH1F *randomHist1 = new TH1F("randomHist1", "Random Histogram;x;frequency", 100, 0, 100);
   TRandom2 *generator=new TRandom2(0);  // parameter == seed, 0->use clock
 
   for (int i=0 ; i<entries ; i++){
@@ -24,10 +25,13 @@ void fit1(int entries=1000, bool save=false) {
   // gStyle->SetOptFit(111);  // show reduced chi2 and params
   gStyle->SetOptFit(1111); // show reduced chi2, probability, and params
   randomHist1->Fit("gaus","");  
-  randomHist1->Draw("e");  // "e" shows bin errors
+  randomHist1->DrawCopy("e");  // "e" shows bin errors
+  // Using DrawCopy vs Draw allows us to delete the original histogram
+  // without removing it from the display.  If we save the histogran to a
+  // file and close the file, it will be deleted from memory.
 
 
-  // Here we used a built in function, gaus, in the fit
+  // Above we used a built in function, gaus, in the fit
   // This function will be associated with the histogram
   // and may be retrieved to get parameter information
   // Refer to http://root.cern.ch/root/html/TF1.html
@@ -38,10 +42,10 @@ void fit1(int entries=1000, bool save=false) {
   cout << fitfunc->GetParameter(0) << " +- " << fitfunc->GetParError(0) << endl;
   cout << fitfunc->GetParameter(1) << " +- " << fitfunc->GetParError(1) << endl;
   cout << fitfunc->GetParameter(2) << " +- " << fitfunc->GetParError(2) << endl;
-  cout << "Fit Probability: " << fitfunc->GetProb() << endl;
+  cout << "Fit Probability: " << fitfunc->GetProb() << endl; // returns chi^2 p-value
 
   if (save) {
     tf->Write();
-    //  tf->Close();
+    tf->Close();
   }
 }
---
+++ fit1.py
@@ -0,0 +1,56 @@
+import ROOT as r
+
+def fit1(entries=1000, save=False):
+    # entries is the number of random samples filled into the histogram
+
+    randomHist1 = r.TH1F("randomHist1", "Random Histogram;x;frequency", 100, 0, 100)
+    generator=r.TRandom2(0)  # parameter == seed, 0->use clock
+
+    for i in range(entries):
+        randomHist1.Fill(generator.Gaus(50,10)) # params: mean, sigma
+
+    #simple fits may be performed automatically
+    r.gStyle.SetOptFit(1111) # show reduced chi2, probability, and params
+    randomHist1.Fit("gaus","")
+    randomHist1.DrawCopy("e")  # "e" shows bin errors
+    # Using DrawCopy vs Draw allows us to delete the original histogram
+    # without removing it from the display.  If we save the histogran to a
+    #file and close the file, it will be deleted from memory.
+
+    # Above we used a built in function, gaus, in the fit
+    # This function will be associated with the histogram
+    # and may be retrieved to get parameter information
+    # Refer to http://root.cern.ch/root/html/TF1.html
+    # for a complete list of TF1 methods
+
+    fitfunc = randomHist1.GetFunction("gaus")
+    print("\nFit Params and errors")
+    for i in range(3):
+        print(f'{fitfunc.GetParameter(i):.2f} +- {fitfunc.GetParError(i):.2f}')
+
+    print(f'Fit Probability: {fitfunc.GetProb():.2f}') # returns chi^2 p-value
+
+    if save:
+        tf.Write()
+        tf.Close()
+
+
+    
+## example of plotting a similar histogram with error bars using numpy/matplotlib
+# import numpy as np
+# from matplotlib import pyplot as plt
+#vals=np.random.normal(loc=50, scale=10, size=entries)
+#y,binEdges=np.histogram(vals, bins=50, range=(1,100))
+#bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
+#stdErr     = np.sqrt(y)
+#width      = binEdges[1]-binEdges[0]
+#plt.bar(bincenters, y, width=width, color='r', yerr=stdErr)
+#plt.show()
+
+
+
+# **************************************
+
+if __name__ == "__main__":
+    fit1()
+    input("hit Enter to exit")
