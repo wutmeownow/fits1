e2e7fd1f0c9e60828f83b5fbf2095e7d96e5d86c
Michael Lowry <mjl9ka@virginia.edu>
2025-10-27 15:21:56 -0400

    changed some things in fit1a.C:

--- fit1a.C
+++ fit1a.C
@@ -8,14 +8,14 @@
 
 // fit1.C
 // entries is the number of random samples filled into the histogram
-void fit1a(int entries=1000, int ntrials=10, bool save=false) {
+void fit1a(int entries=1000, int ntrials=1000, bool save=false) {
    //Simple histogram fitting examples
   gROOT->Reset();  // useful to reset ROOT to a cleaner state
 
   TFile *tf=0;
   if (save) tf=new TFile("histo.root","recreate");
 
-  TH1F *chi2 = new TH1F("randomHist1", "Random Histogram", 100, 0, 100);
+  TH1F *redchi2 = new TH1F("redchisqr", "Reduced Chi Sqr", 100, 0, 5);
   TH1F *randomHist1 = new TH1F("randomHist1", "Random Histogram", 100, 0, 100);
   TRandom2 *generator=new TRandom2(0);  // parameter == seed, 0->use clock
 
@@ -24,17 +24,18 @@ void fit1a(int entries=1000, int ntrials=10, bool save=false) {
     for (int i=0 ; i<entries ; i++){
         randomHist1->Fill(generator->Gaus(50,10)); // params: mean, sigma
       }
-    randomHist1->Fit("gaus","");
+    randomHist1->Fit("gaus","Q");
     // do more stuff
     TF1 *fitfunc = randomHist1->GetFunction("gaus");
-    cout << "\nFit Params and errors" << endl;
-    cout << fitfunc->GetParameter(0) << " +- " << fitfunc->GetParError(0) << endl;
-    cout << fitfunc->GetParameter(1) << " +- " << fitfunc->GetParError(1) << endl;
-    cout << fitfunc->GetParameter(2) << " +- " << fitfunc->GetParError(2) << endl;
-    cout << "Fit Probability: " << fitfunc->GetProb() << endl; // returns chi^2 p-value
-    cout << "Fit Chi Square: " << fitfunc->GetChisquare() << endl; // returns chi^2
-    cout << "Fit NDF: " << fitfunc->GetNDF() << endl; // returns # DoF
-    cout << "Fit Reduced Chi Square: " << fitfunc->GetChisquare()/fitfunc->GetNDF() << endl; // returns # DoF
+    // cout << "\nFit Params and errors" << endl;
+    // cout << fitfunc->GetParameter(0) << " +- " << fitfunc->GetParError(0) << endl;
+    // cout << fitfunc->GetParameter(1) << " +- " << fitfunc->GetParError(1) << endl;
+    // cout << fitfunc->GetParameter(2) << " +- " << fitfunc->GetParError(2) << endl;
+    // cout << "Fit Probability: " << fitfunc->GetProb() << endl; // returns chi^2 p-value
+    // cout << "Fit Chi Square: " << fitfunc->GetChisquare() << endl; // returns chi^2
+    // cout << "Fit NDF: " << fitfunc->GetNDF() << endl; // returns # DoF
+    // cout << "Fit Reduced Chi Square: " << fitfunc->GetChisquare()/fitfunc->GetNDF() << endl; // returns # DoF
+    redchi2->Fill(fitfunc->GetChisquare()/fitfunc->GetNDF());
   }
 
   // simple fits may be performed automatically
